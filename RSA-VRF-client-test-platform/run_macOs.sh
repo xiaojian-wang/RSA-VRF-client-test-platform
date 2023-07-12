1d846423de00b3b74a6d5437645e1e218be50413
Xiaojian Wang <461005496@qq.com>
2023-07-12 14:18:39 -0400

    Create run_macOs.sh

---
+++ RSA-VRF-client-test-platform/run_macOs.sh
@@ -0,0 +1,24 @@
+for ((j=1; j<=1000; j++)); 
+do
+    python choose_verifier.py -n 10
+done
+
+for ((j=1; j<=1000; j++)); 
+do
+    python choose_verifier.py -n 20
+done
+
+for ((j=1; j<=1000; j++)); 
+do
+    python choose_verifier.py -n 30
+done
+
+for ((j=1; j<=1000; j++)); 
+do
+    python choose_verifier.py -n 40
+done
+
+for ((j=1; j<=1000; j++)); 
+do
+    python choose_verifier.py -n 50
+done
